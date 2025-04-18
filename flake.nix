cfb4b8fd5fa085af5c60e8cd5e7e195ecbb67b62
100001 <github.ixda@8shield.net>
2025-04-18 13:14:29 -0600

    Pin nixpkgs and allow unfree

--- flake.nix
+++ flake.nix
@@ -5,7 +5,10 @@
 
   outputs = { self, nixpkgs }: let
     system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
+    pkgs   = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    };
   in {
     packages.${system}.crealityprint =
       pkgs.callPackage ./crealityprint.nix { };
