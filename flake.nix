b2a2819dc8187c6982088212c9b0657fe89358e9
100001 <github.ixda@8shield.net>
2025-04-18 13:36:00 -0600

    adding some stuff updates creality print

--- flake.nix
+++ flake.nix
@@ -1,24 +1,25 @@
-# flake.nix
-inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";   # or a commit hash
-
-outputs = { self, nixpkgs }:
-let
-  eachSystem = nixpkgs.lib.genAttrs [ "x86_64-linux" ];
-  pkgsFor = system: import nixpkgs {
-    inherit system;
-    config.allowUnfree = true;
-  };
-in {
-  packages = eachSystem (sys: {
-    crealityprint = (pkgsFor sys).callPackage ./crealityprint.nix { };
-  });
-  defaultPackage = eachSystem (sys: self.packages.${sys}.crealityprint);
-  apps = eachSystem (sys: {
-    crealityprint = {
-      type = "app";
-      program = "${self.packages.${sys}.crealityprint}/bin/crealityprint";
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";   # or a commit hash
+  
+  outputs = { self, nixpkgs }:
+  let
+    eachSystem = nixpkgs.lib.genAttrs [ "x86_64-linux" ];
+    pkgsFor = system: import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
     };
-  });
-  defaultApp = eachSystem (sys: self.apps.${sys}.crealityprint);
-};
+  in {
+    packages = eachSystem (sys: {
+      crealityprint = (pkgsFor sys).callPackage ./crealityprint.nix { };
+    });
+    defaultPackage = eachSystem (sys: self.packages.${sys}.crealityprint);
+    apps = eachSystem (sys: {
+      crealityprint = {
+        type = "app";
+        program = "${self.packages.${sys}.crealityprint}/bin/crealityprint";
+      };
+    });
+    defaultApp = eachSystem (sys: self.apps.${sys}.crealityprint);
+  };
+}
 
