8c86a9d91dc7e16fd05b436396997151e588e256
100001 <github.ixda@8shield.net>
2025-04-18 14:08:32 -0600

    Allow unfree, drop webkitgtk_4_0, correct wrapper path

--- crealityprint.nix
+++ crealityprint.nix
@@ -1,11 +1,11 @@
-{
-  lib,
-  fetchurl,
-  appimageTools,
-  makeDesktopItem,
+{ lib
+, fetchurl
+, appimageTools
+, makeDesktopItem
 }:
+
 let
-  pname = "crealityprint";
+  pname   = "crealityprint";
   version = "6.1.0-beta";
 
   src = fetchurl {
@@ -13,59 +13,44 @@ let
     sha256 = "sha256-54oR0pnpUjkHKc3FBjFeUvpU4VeTxd6guJH1cOLV5lw=";
   };
 
-  extracted = appimageTools.extractType2 {inherit pname version src;};
+  extracted = appimageTools.extractType2 { inherit pname version src; };
 
   desktopItem = makeDesktopItem {
-    name = pname;
-    exec = pname;
+    name        = pname;
+    exec        = pname;            # wrapper created by wrapType2
     desktopName = "CrealityPrint";
-    comment = "3‑D printer slicer by Creality";
-    icon = "CrealityPrint"; # matches upstream filename
-    categories = ["Graphics" "3DGraphics" "Engineering"];
+    comment     = "3‑D printer slicer by Creality";
+    icon        = "CrealityPrint";
+    categories  = [ "Graphics" "3DGraphics" "Engineering" ];
   };
 in
-  appimageTools.wrapType2 {
-    inherit pname version src;
+appimageTools.wrapType2 {
+  inherit pname version src;
 
-    extraPkgs = pkgs:
-      with pkgs; [
-        zlib
-        glib
-        gtk3
-        pango
-        cairo
-        gdk-pixbuf
-        libsecret
-        mesa
-        xorg.libX11
-        xorg.libxcb
-        xorg.libXrandr
-        xorg.libXi
-        libxkbcommon
-        wayland
-        ffmpeg
-        webkitgtk_4_1
-      ];
+  extraPkgs = pkgs: with pkgs; [
+    zlib glib gtk3 pango cairo gdk-pixbuf libsecret mesa
+    xorg.libX11 xorg.libxcb xorg.libXrandr xorg.libXi libxkbcommon wayland
+    ffmpeg
+  ];
 
-    extraInstallCommands = ''
-      # Desktop entry
-      install -Dm644 ${desktopItem}/share/applications/${pname}.desktop \
-        $out/share/applications/${pname}.desktop
+  extraInstallCommands = ''
+    install -Dm644 ${desktopItem}/share/applications/${pname}.desktop \
+      $out/share/applications/${pname}.desktop
 
-      # Copy whatever icons the AppImage ships
-      if [ -d ${extracted}/usr/share/icons/hicolor ]; then
-        cp -r --no-preserve=mode,ownership \
-          ${extracted}/usr/share/icons/hicolor \
-          $out/share/icons/
-      fi
-    '';
+    # copy every icon size shipped in the AppImage
+    if [ -d ${extracted}/usr/share/icons/hicolor ]; then
+      cp -r --no-preserve=mode,ownership \
+        ${extracted}/usr/share/icons/hicolor \
+        $out/share/icons/
+    fi
+  '';
 
-    meta = with lib; {
-      description = "Creality Print 3‑D slicer";
-      homepage = "https://github.com/jamincollins/CrealityPrint";
-      license = licenses.unfree;
-      maintainers = with maintainers; [];
-      platforms = ["x86_64-linux"];
-      mainProgram = pname;
-    };
-  }
+  meta = with lib; {
+    description = "Creality Print 3‑D slicer";
+    homepage    = "https://github.com/jamincollins/CrealityPrint";
+    license     = licenses.unfree;
+    platforms   = [ "x86_64-linux" ];
+    mainProgram = pname;
+  };
+}
+
--- flake.nix
+++ flake.nix
@@ -1,32 +1,37 @@
 {
   description = "CrealityPrint AppImage packaged for Nix";
 
+  ############################################################
+  # Inputs (pin nixpkgs so nix never tries to write a lock)
+  ############################################################
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: let
-    eachSystem = nixpkgs.lib.genAttrs ["x86_64-linux"];
-    pkgsFor = system:
-      import nixpkgs {
-        inherit system;
-        config.allowUnfree = true; # <- add this line
-      };
-  in {
+  ############################################################
+  # Outputs
+  ############################################################
+  outputs = { self, nixpkgs }:
+  let
+    eachSystem = nixpkgs.lib.genAttrs [ "x86_64-linux" ];
+    pkgsFor = system: import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;        # ← users need no env vars / flags
+    };
+  in
+  {
+    ######################  Packages  #######################
     packages = eachSystem (sys: {
-      crealityprint = (pkgsFor sys).callPackage ./crealityprint.nix {};
+      crealityprint = (pkgsFor sys).callPackage ./crealityprint.nix { };
     });
-
     defaultPackage = eachSystem (sys: self.packages.${sys}.crealityprint);
 
+    ########################  Apps  #########################
     apps = eachSystem (sys: {
       crealityprint = {
         type = "app";
         program = "${self.packages.${sys}.crealityprint}/bin/crealityprint";
       };
     });
-
     defaultApp = eachSystem (sys: self.apps.${sys}.crealityprint);
   };
 }
+
