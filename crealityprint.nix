c3b5c1df95cdcd9ecf79501d593b8ec375844253
100001 <github.ixda@8shield.net>
2025-05-04 17:33:45 -0600

    update to 6.1.1

--- crealityprint.nix
+++ crealityprint.nix
@@ -1,56 +1,70 @@
-{ lib
-, fetchurl
-, appimageTools
-, makeDesktopItem
+{
+  lib,
+  fetchurl,
+  appimageTools,
+  makeDesktopItem,
 }:
-
 let
-  pname   = "crealityprint";
-  version = "6.1.0-beta";
+  pname = "crealityprint";
+  version = "6.1.1";
 
   src = fetchurl {
-    url = "https://github.com/jamincollins/CrealityPrint/releases/download/v6.1.0-beta1/CrealityPrint-V6.1.0-x86_64-Beta.AppImage";
-    sha256 = "sha256-54oR0pnpUjkHKc3FBjFeUvpU4VeTxd6guJH1cOLV5lw=";
+    url = "https://github.com/jamincollins/CrealityPrint/releases/download/v${version}-beta1/CrealityPrint-V${version}-x86_64-Beta.AppImage";
+    sha256 = "sha256-l42gFtJdn0JQg41V2fzGWjf1fN6ijF7Z0iZM6hEt9gM=";
   };
 
-  extracted = appimageTools.extractType2 { inherit pname version src; };
+  extracted = appimageTools.extractType2 {
+    inherit pname version src;
+  };
 
   desktopItem = makeDesktopItem {
-    name        = pname;
-    exec        = pname;            # wrapper created by wrapType2
+    name = pname;
+    exec = pname;
     desktopName = "CrealityPrint";
-    comment     = "3‑D printer slicer by Creality";
-    icon        = "CrealityPrint";
-    categories  = [ "Graphics" "3DGraphics" "Engineering" ];
+    comment = "3‑D Printer slicer by Creality";
+    icon = "CrealityPrint"; # matches upstream filename
+    categories = ["Graphics" "3DGraphics" "Engineering"];
   };
 in
-appimageTools.wrapType2 {
-  inherit pname version src;
+  appimageTools.wrapType2 {
+    inherit pname version src;
 
-  extraPkgs = pkgs: with pkgs; [
-    zlib glib gtk3 pango cairo gdk-pixbuf libsecret mesa
-    xorg.libX11 xorg.libxcb xorg.libXrandr xorg.libXi libxkbcommon wayland
-    ffmpeg webkitgtk_4_1 glib-networking
-  ];
+    extraPkgs = pkgs:
+      with pkgs; [
+        zlib
+        glib
+        gtk3
+        pango
+        cairo
+        gdk-pixbuf
+        libsecret
+        mesa
+        xorg.libX11
+        xorg.libxcb
+        xorg.libXrandr
+        xorg.libXi
+        libxkbcommon
+        wayland
+        ffmpeg
+        webkitgtk_4_0
+      ];
 
-  extraInstallCommands = ''
-    install -Dm644 ${desktopItem}/share/applications/${pname}.desktop \
-      $out/share/applications/${pname}.desktop
+    extraInstallCommands = ''
+      install -Dm644 ${desktopItem}/share/applications/${pname}.desktop \
+        $out/share/applications/${pname}.desktop
 
-    # copy every icon size shipped in the AppImage
-    if [ -d ${extracted}/usr/share/icons/hicolor ]; then
-      cp -r --no-preserve=mode,ownership \
-        ${extracted}/usr/share/icons/hicolor \
-        $out/share/icons/
-    fi
-  '';
+      if [ -d ${extracted}/usr/share/icons/hicolor ]; then
+        cp -r --no-preserve=mode,ownership ${extracted}/usr/share/icons/hicolor \
+          $out/share/icons/
+      fi
+    '';
 
-  meta = with lib; {
-    description = "Creality Print 3‑D slicer";
-    homepage    = "https://github.com/jamincollins/CrealityPrint";
-    license     = licenses.unfree;
-    platforms   = [ "x86_64-linux" ];
-    mainProgram = pname;
-  };
-}
-
+    meta = with lib; {
+      description = "Creality Print 3‑D slicer";
+      homepage = "https://github.com/jamincollins/CrealityPrint";
+      license = licenses.unfree;
+      maintainers = with maintainers; [];
+      platforms = ["x86_64-linux"];
+      mainProgram = pname;
+    };
+  }
